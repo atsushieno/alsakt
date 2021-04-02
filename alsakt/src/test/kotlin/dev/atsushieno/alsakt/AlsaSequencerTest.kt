4f384e88bf0c18ee7e7a38d136b1ad3a41f97f68
atsushieno <atsushieno@gmail.com>
2021-04-02 14:06:16 +0900

    GH Actions does not come up with /dev/snd/seq, so skip tests that expects it.

--- alsakt/src/test/kotlin/dev/atsushieno/alsakt/AlsaSequencerTest.kt
+++ alsakt/src/test/kotlin/dev/atsushieno/alsakt/AlsaSequencerTest.kt
@@ -1,10 +1,15 @@
 package dev.atsushieno.alsakt
 
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class AlsaSequencerTest {
     @Test
     fun simpleUse() {
+        if (!File("/dev/snd/seq").exists()) {
+            return
+        }
+
         val seq = AlsaSequencer(AlsaIOType.Duplex, AlsaIOMode.NonBlocking)
         seq.close()
     }
